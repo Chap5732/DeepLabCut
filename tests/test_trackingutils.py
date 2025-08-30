27ffa988ba759773de2fa219419d59a09ac2ec5f
Chap5732 <144191386+Chap5732@users.noreply.github.com>
2025-08-30 19:57:59 +0800

    Avoid matching gated-out ellipses

--- tests/test_trackingutils.py
+++ tests/test_trackingutils.py
@@ -75,6 +75,21 @@ def _ellipse_pose(offset):
     return base + np.asarray(offset)
 
 
+@pytest.mark.parametrize("gate_kwargs", [{"max_px": 5}, {"v_gate_pxpf": 5}])
+def test_sort_ellipse_gates_zero_iou(gate_kwargs):
+    mot_tracker = trackingutils.SORTEllipse(5, 1, 0, **gate_kwargs)
+    pose = _ellipse_pose((0, 0))[None, ...]
+    mot_tracker.track(pose)
+    far_pose = _ellipse_pose((10, 10))[None, ...]
+    ret = mot_tracker.track(far_pose)
+    assert ret.size == 0
+    assert len(mot_tracker.trackers) == 2
+    assert mot_tracker.trackers[0].time_since_update == 1
+    ret = mot_tracker.track(pose)
+    assert ret.shape[0] == 1
+    assert ret[0, -2] == 0
+
+
 def test_sort_ellipse_max_px_gate():
     mot_tracker = trackingutils.SORTEllipse(5, 1, 0.1, max_px=5)
     pose = _ellipse_pose((0, 0))[None, ...]
